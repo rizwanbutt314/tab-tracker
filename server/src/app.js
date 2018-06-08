af6cc716a01f27e7903727a8539a99fa8299956f
rizwan-hameed <rbutt@sparkcognition.com>
2018-06-08 11:33:47 +0500

    server file updated with configurations and endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
 
-console.log('Hello')
+// App intialization
+const app = express()
+
+// App configuration
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// Endpoints
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'Hello World!'
+  })
+})
+
+// Server listening
+app.listen(process.env.PORT || 8081)
