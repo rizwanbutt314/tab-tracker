cfee6f73882cd7c1494acf381989d4589555a81c
rizwan-hameed <rbutt@sparkcognition.com>
2018-06-19 12:43:15 +0500

    vuetify theme added in register section

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,21 +1,31 @@
 <template>
-  <div>
-      <h1>Register</h1>
 
-    <input
-      name='email'
-      type='email'
-      v-model='email'
-      palceholder='Email'/>
-    <br>
-    <input
-      name='password'
-      type='password'
-      v-model='password'
-      palceholder='password'/>
-    <br>
-    <button @click="register()">Register</button>
-  </div>
+  <v-layout>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+
+        </div>
+          <input
+            name='email'
+            type='email'
+            v-model='email'
+            palceholder='Email'/>
+          <br>
+          <input
+            name='password'
+            type='password'
+            v-model='password'
+            palceholder='password'/>
+          <br>
+          <div class="err" v-html="error"/>
+          <v-btn class="cyan" @click="register()">Register</v-btn>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -24,16 +34,21 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+        console.log(response.data)
+      } catch (e) {
+        this.error = e.response.data.error
+      }
     }
   },
   // watch: {
@@ -51,5 +66,7 @@ export default {
 
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
-
+.err{
+  color: red;
+}
 </style>
